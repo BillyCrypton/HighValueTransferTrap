a4eb81948d46076898e26013560dda6ea5672ff7
BillyCrypton <240mbbs12@gmail.com>
2025-10-14 21:35:05 +0100

    Update DeployTrap.s.sol

--- script/DeployTrap.s.sol
+++ script/DeployTrap.s.sol
@@ -3,26 +3,22 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
 import "../src/HighValueTransferTrap.sol";
-import "../src/HighValueTransferResponse.sol";
 
 contract DeployTrap is Script {
     function run() external {
-        uint256 pk = vm.envUint("PRIVATE_KEY");
+        // Load private key from environment variables
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
-        vm.startBroadcast(pk);
+        // Response contract address (already deployed)
+        address RESPONSE_CONTRACT = 0xD7D61ceF2D05a1485a408d7B683156B7D4E6b8BE;
 
-        // Deploy response first
-        HighValueTransferResponse resp = new HighValueTransferResponse();
+        vm.startBroadcast(deployerPrivateKey);
 
-        // Deploy trap with response address
-        HighValueTransferTrap trap = new HighValueTransferTrap(address(resp));
-
-        // Optionally set a sample threshold (owner is deployer)
-        // trap.setThreshold(0x...tokenAddress..., 1000000000000000000);
-
-        console.log("Deployed Response at:", address(resp));
-        console.log("Deployed Trap at:", address(trap));
+        // Deploy the HighValueTransferTrap contract
+        HighValueTransferTrap trap = new HighValueTransferTrap(RESPONSE_CONTRACT);
 
         vm.stopBroadcast();
+
+        console.log("HighValueTransferTrap deployed at:", address(trap));
     }
 }
