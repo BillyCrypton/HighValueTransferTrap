f8df2b4ec9588ca97df9cb7cf5a86a9e49d2c3a1
BillyCrypton <240mbbs12@gmail.com>
2025-10-16 11:05:30 +0100

    Update DeployTrap.s.sol

--- script/DeployTrap.s.sol
+++ script/DeployTrap.s.sol
@@ -6,19 +6,12 @@ import "../src/HighValueTransferTrap.sol";
 
 contract DeployTrap is Script {
     function run() external {
-        // Load private key from environment variables
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-
-        // Response contract address (already deployed)
-        address RESPONSE_CONTRACT = 0xD7D61ceF2D05a1485a408d7B683156B7D4E6b8BE;
-
         vm.startBroadcast(deployerPrivateKey);
 
-        // Deploy the HighValueTransferTrap contract
-        HighValueTransferTrap trap = new HighValueTransferTrap(RESPONSE_CONTRACT);
+        HighValueTransferTrap trap = new HighValueTransferTrap();
+        console.log("HighValueTransferTrap deployed to:", address(trap));
 
         vm.stopBroadcast();
-
-        console.log("HighValueTransferTrap deployed at:", address(trap));
     }
 }
